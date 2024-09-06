c2326090292e8b7e430fc027ef596c9c7da47d8a
Bruno Pacheco <148828825+BrunoPS295@users.noreply.github.com>
2024-09-06 17:09:15 -0300

    Atualização do horas e + repetição.js

---
+++ Curso_em_video/Exercicios/ex6/repetição.js
@@ -0,0 +1,22 @@
+//Teste logico no inicio
+
+n = 10
+while(n >= 0){
+    console.log(`n = ${n}`)
+    n--
+}
+
+//teste logico no final
+
+var m = 0;
+do{
+    console.log(`m = ${m}`)
+    m++
+}while(m <= 10)
+
+// Utilizar o for é como se você simplificasse os codigos acima onde na primeira parte seria a variavel, a segunda seria a condição, e a terceira o que fazer se condição for verdadeira
+
+//Utilizando for
+for(c=0;c <= 10;c++){
+    console.log(`c = ${c}`)
+}
